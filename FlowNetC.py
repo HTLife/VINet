d1cf282aeed28a81ec79440d76e9b46c31ed224e
JackyLiu <jjkka132@gmail.com>
2018-04-23 06:35:32 +0000

    add expriment code

--- FlowNetC.py
+++ FlowNetC.py
@@ -73,9 +73,9 @@ class FlowNetC(nn.Module):
         x1 = x[:,0:3,:,:]
         x2 = x[:,3::,:,:]
 
-        out_conv1a = self.conv1(x1)
-        out_conv2a = self.conv2(out_conv1a)
-        out_conv3a = self.conv3(out_conv2a)
+        out_conv1a = self.conv1(x1) # ==> [5, 64, 192, 256]
+        out_conv2a = self.conv2(out_conv1a) # ==> [5, 128, 96, 128]
+        out_conv3a = self.conv3(out_conv2a) # ==> [5, 256, 48, 64]
 
         # FlownetC bottom input stream
         out_conv1b = self.conv1(x2)
@@ -84,47 +84,47 @@ class FlowNetC(nn.Module):
         out_conv3b = self.conv3(out_conv2b)
 
         # Merge streams
-        out_corr = self.corr(out_conv3a, out_conv3b) # False
+        out_corr = self.corr(out_conv3a, out_conv3b) # False   # ==> [5, 441, 48, 64]
         out_corr = self.corr_activation(out_corr)
 
         # Redirect top input stream and concatenate
-        out_conv_redir = self.conv_redir(out_conv3a)
-
-        in_conv3_1 = torch.cat((out_conv_redir, out_corr), 1)
+        out_conv_redir = self.conv_redir(out_conv3a)  # ==> [5, 32, 48, 64]
+        
+        in_conv3_1 = torch.cat((out_conv_redir, out_corr), 1) # ==> [5, 473, 48, 64]
 
         # Merged conv layers
-        out_conv3_1 = self.conv3_1(in_conv3_1)
+        out_conv3_1 = self.conv3_1(in_conv3_1) # ==> [5, 256, 48, 64]
+        out_conv4 = self.conv4_1(self.conv4(out_conv3_1)) # ==> [5, 512, 24, 32]
+        
+        
+        out_conv5 = self.conv5_1(self.conv5(out_conv4)) # ==> [5, 512, 12, 16]
+        out_conv6 = self.conv6_1(self.conv6(out_conv5)) # ==> [5, 1024, 6, 8]
 
-        out_conv4 = self.conv4_1(self.conv4(out_conv3_1))
+#         flow6       = self.predict_flow6(out_conv6) # ==> [5, 2, 6, 8]
+#         flow6_up    = self.upsampled_flow6_to_5(flow6) # ==> [5, 2, 12, 16]
+#         out_deconv5 = self.deconv5(out_conv6)
 
-        out_conv5 = self.conv5_1(self.conv5(out_conv4))
-        out_conv6 = self.conv6_1(self.conv6(out_conv5))
+#         concat5 = torch.cat((out_conv5,out_deconv5,flow6_up),1)
 
-        flow6       = self.predict_flow6(out_conv6)
-        flow6_up    = self.upsampled_flow6_to_5(flow6)
-        out_deconv5 = self.deconv5(out_conv6)
+#         flow5       = self.predict_flow5(concat5)
+#         flow5_up    = self.upsampled_flow5_to_4(flow5)
+#         out_deconv4 = self.deconv4(concat5)
+#         concat4 = torch.cat((out_conv4,out_deconv4,flow5_up),1)
 
-        concat5 = torch.cat((out_conv5,out_deconv5,flow6_up),1)
+#         flow4       = self.predict_flow4(concat4)
+#         flow4_up    = self.upsampled_flow4_to_3(flow4)
+#         out_deconv3 = self.deconv3(concat4)
+#         concat3 = torch.cat((out_conv3_1,out_deconv3,flow4_up),1)
 
-        flow5       = self.predict_flow5(concat5)
-        flow5_up    = self.upsampled_flow5_to_4(flow5)
-        out_deconv4 = self.deconv4(concat5)
-        concat4 = torch.cat((out_conv4,out_deconv4,flow5_up),1)
+#         flow3       = self.predict_flow3(concat3)
+#         flow3_up    = self.upsampled_flow3_to_2(flow3)
+#         out_deconv2 = self.deconv2(concat3)
+#         concat2 = torch.cat((out_conv2a,out_deconv2,flow3_up),1)
 
-        flow4       = self.predict_flow4(concat4)
-        flow4_up    = self.upsampled_flow4_to_3(flow4)
-        out_deconv3 = self.deconv3(concat4)
-        concat3 = torch.cat((out_conv3_1,out_deconv3,flow4_up),1)
-
-        flow3       = self.predict_flow3(concat3)
-        flow3_up    = self.upsampled_flow3_to_2(flow3)
-        out_deconv2 = self.deconv2(concat3)
-        concat2 = torch.cat((out_conv2a,out_deconv2,flow3_up),1)
-
-        flow2 = self.predict_flow2(concat2)
+#         flow2 = self.predict_flow2(concat2)
 
         #if self.training:
         #    return flow2,flow3,flow4,flow5,flow6
         #else:
         #    return flow2,
-        return out_conv6 # (1 , 1024, 6, 8)
+        return out_conv6 # (5 , 1024, 6, 8)
